e1d7cac3528201d386708c9488b0ba3178ecd7a1
son <handsun000@gmail.com>
2024-12-24 14:51:51 +0900

    postController 1

--- src/main/java/com/ll/restByTdd/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/restByTdd/domain/post/post/service/PostService.java
@@ -1,11 +1,36 @@
 package com.ll.restByTdd.domain.post.post.service;
 
+import com.ll.restByTdd.domain.post.post.entity.Post;
 import com.ll.restByTdd.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+
+    public long count() {
+        return postRepository.count();
+    }
+
+    public Post write(String title, String content) {
+
+        Post post = Post.builder()
+                .title(title)
+                .content(content)
+                .build();
+
+        return postRepository.save(post);
+    }
+
+    public List<Post> findAllByOrderByIdDesc() {
+        return postRepository.findAllByOrderByIdDesc();
+    }
+
+    public Post findById(long id) {
+        return postRepository.findById(id).get();
+    }
 }
--- src/main/java/com/ll/restByTdd/global/initData/BaseInitData.java
+++ src/main/java/com/ll/restByTdd/global/initData/BaseInitData.java
@@ -1,5 +1,7 @@
 package com.ll.restByTdd.global.initData;
 
+import com.ll.restByTdd.domain.post.post.repository.PostRepository;
+import com.ll.restByTdd.domain.post.post.service.PostService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,23 +13,26 @@ import org.springframework.context.annotation.Lazy;
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
-    @Autowired
+
+    private final PostService postService;
+
     @Lazy
+    @Autowired
     private BaseInitData self;
 
     @Bean
     public ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
             self.work1();
-            self.work2();
         };
     }
+
     @Transactional
     public void work1() {
-        System.out.println("work1");
-    }
-    @Transactional
-    public void work2() {
-        System.out.println("work2");
+        if (postService.count() > 0) return;
+
+        postService.write("축구 하실 분?", "14시 까지 22명을 모아오세요");
+        postService.write("농구 하실 분?", "15시 까지 12명을 모아오세요");
+        postService.write("배구 하실 분?", "16시 까지 10명을 모아오세요");
     }
 }

cac2a3319fc59b37f01cca5621467573249f329c
son <handsun000@gmail.com>
2024-12-24 14:25:20 +0900

    post1

---
+++ src/main/java/com/ll/restByTdd/domain/post/post/entity/Post.java
@@ -0,0 +1,21 @@
+package com.ll.restByTdd.domain.post.post.entity;
+
+import com.ll.restByTdd.global.initData.jpa.entity.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post extends BaseTime {
+
+    @Column(length = 100)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+}
